abe589e834549f85d7b34744498bcca7e51b6c78
Stets <EStets@kiev.luxoft.com>
2018-03-26 17:58:05 +0300

    added function to check if line is a phone number

--- Hello.java
+++ Hello.java
@@ -1,1 +1,9 @@
-Hello for everyone.
+public class Hello{
+	public static void main(String[] args){
+		System.out.prinl("hello");
+	}
+	public boolean isPhoneNumber(String line){
+		return false;
+	}
+	
+}
